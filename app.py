3b2393f11f1bce3e310199fb216e1621d46dd8dd
Aniket <108399480+Aniket2021448@users.noreply.github.com>
2024-06-30 13:34:34 +0530

    App created, starting modifications

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import time
+import dotenv
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
 from dotenv import load_dotenv
@@ -11,7 +13,7 @@ from langchain.vectorstores import Pinecone
 import pinecone
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.llms import CTransformers
 
 
@@ -20,7 +22,7 @@ st.set_page_config(page_title= "Medical chatbot", page_icon=":bot:")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
-
+PINECONE_API_KEY = "1bae0d8e-019e-4e87-8080-ecf523e5f25f"
 def get_response(user_query):
     # Initilize the prompt
     # create prompt template, integrate chatHistory component as well
@@ -38,19 +40,31 @@ def get_response(user_query):
     PROMPT = PromptTemplate(template = prompt_template, input_variables=["context", "question"])
     chain_type_kwargs = {"prompt":PROMPT}
 
-    llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin", model_type="llama", config={'max_new_tokens': 512, 'temperature': 1})
-    index = pinecone.Index(name="medical-chatbot")
+    llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin", model_type="llama", config={'max_new_tokens': 1024, 'temperature': 1})
+    
+    index_name = "medical-chatbot"
+    index=pinecone.Index(api_key=PINECONE_API_KEY, host="https://medical-chatbot-pv4ded8.svc.aped-4627-b74a.pinecone.io")
+
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     # Create Pinecone retriever
     vector_store = Pinecone(index, embeddings, text_key="text")
 
     
     qa = RetrievalQA.from_chain_type(llm, chain_type="stuff",retriever = vector_store.as_retriever(search_kwargs={"k": 2}), chain_type_kwargs=chain_type_kwargs)
+    answer = qa.invoke({"query":user_query})
 
-    # query = "How to cure AIDS?"
-    answer = qa.invoke(user_query)
-    return answer.result
+    return answer
+    # answer = vector_store.similarity_search(user_query, k=3)
+    # return answer.stream().get("answer")
 
+# Function to simulate typing effect
+def type_effect(text):
+    for char in text:
+        st.write(char)
+        time.sleep(0.05)
+    st.write("")
+
+    
 st.title("Medical chatbot")
 
 st.write("Welcome to the medical chatbot. Please enter your symptoms below and I will try to help you.")
@@ -74,7 +88,12 @@ if user_query is not None and user_query != "":
 
     with st.chat_message("AI"):
         # ai_message = HumanMessage(user_query)
-        ai_response = "I don;t know"
-        st.markdown(ai_response)
+        # # ai_response = ["result"]
+        ai_response = get_response(user_query)
+        # st.write(type(ai_response))
+        result = ai_response["result"]
+        # type_effect(result)
+        st.markdown(result)
+
         # Get the response from backend and present it here
-        st.session_state.chat_history.append({"bot": ai_response})
+        st.session_state.chat_history.append({"bot": result})
